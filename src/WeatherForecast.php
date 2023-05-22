42bafe4ea0c352f50218b4de2f56e3064afa1555
Fabian Lüders <info@fabian-lueders.eu>
2023-05-22 19:22:59 +0200

    Intitial setup

---
+++ src/WeatherForecast.php
@@ -0,0 +1,14 @@
+<?php
+namespace Fabia\It8951InfoDisplayImage;
+use Cmfcmf\OpenWeatherMap;
+
+
+class WeatherForecast{
+
+    private OpenWeatherMap $owm;
+
+    function __construct(){
+        //$owm = new OpenWeatherMap(...);
+    }
+
+}
