5afee4d2c51b7fdae3eb07874b943e3bd0036624
Fabian Lüders <info@fabian-lueders.eu>
2023-05-22 20:09:32 +0200

    small update

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
- 
+use It8951InfoDisplayImage\WeatherForecast;
+
+$weather = new WeatherForecast(); 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?=date('Y-m-d H:i:s')?></h1>
+    <?= $weather->getWeatherString()?>
+</body>
+</html>
--- src/WeatherForecast.php
+++ src/WeatherForecast.php
@@ -1,14 +1,29 @@
 <?php
-namespace Fabia\It8951InfoDisplayImage;
+namespace It8951InfoDisplayImage;
+
 use Cmfcmf\OpenWeatherMap;
+use Http\Adapter\Guzzle7\Client;
+use Http\Factory\Guzzle\RequestFactory;
+use Psr\Http\Message\RequestFactoryInterface;
+use Symfony\Component\Dotenv\Dotenv;
 
 
-class WeatherForecast{
-
+class WeatherForecast
+{
     private OpenWeatherMap $owm;
+    private Dotenv $dotenv = new Dotenv();
+    private RequestFactoryInterface $httpRequestFactory = new RequestFactory();
+    private Client $httpClient = Client::createWithConfig([]);
 
-    function __construct(){
-        //$owm = new OpenWeatherMap(...);
+    public function __construct()
+    {
+        $this->dotenv->load(__DIR__ . '/../.env');
+        $this->owm = new OpenWeatherMap($_ENV['OPEN_WHEATER_API_KEY'], $this->httpClient, $this->httpRequestFactory, );
+    }
+
+    public function getWeatherString(): string {
+        $forecast = $this->owm->getWeatherForecast($_ENV['CITY'], $_ENV['UNITS'], $_ENV['LANG'], '', 5);
+        return $forecast->current()->weather->icon;
     }
 
 }
